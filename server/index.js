719e5333ff0b03b663492829e8c93d72eb18703e
balajin96 <balajinatarajan18011996@gmail.com>
2026-04-07 06:45:44 +0530

    11

--- server/index.js
+++ server/index.js
@@ -9,21 +9,21 @@ dotenv.config();
 dbconnection();
 
 //middelware
-// const corsOptions = { origin: true, credentials: true };
-// app.use(cors(corsOptions));
-// app.options('*', cors(corsOptions));
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://client-crudoperation.vercel.app/"
+        "https://client-crudoperation.vercel.app"
     ],
     credentials: true
 }));
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-//routes
-// console.log(app.post);
 
+// handle preflight
+app.options('*', cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
 app.use('/api/createUserData/', router)
 app.use('/api/getUserData/', router);
 app.use('/api/modifyUserData/', router);
@@ -33,6 +33,10 @@ app.use('/api/', router)
 app.use('/api/', router)
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => { console.log(`backend is running ${PORT}`) });
+if (!process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`backend is running ${PORT}`);
+  });
+}
 
 export default app;
